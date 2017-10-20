fc8e5c93e36a842566fd10e9a4dfc2e1967ebaee
marcus sintome <marcus.sintome@toop.com.au>
2017-10-20 21:50:54 +1030

    First Upload

---
+++ PhotoCollectionSaver/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoCollectionSaver
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            if (args.Length > 0)
+            {
+                string firstArgument = args[0].ToLower().Trim();
+                string secondArgument = null;
+
+                // Handle cases where arguments are separated by colon.
+                // Examples: /c:1234567 or /P:1234567
+                if (firstArgument.Length > 2)
+                {
+                    secondArgument = firstArgument.Substring(3).Trim();
+                    firstArgument = firstArgument.Substring(0, 2);
+                }
+                else if (args.Length > 1)
+                    secondArgument = args[1];
+
+                if (firstArgument == "/c")           // Configuration mode
+                {
+                    // TODO
+                }
+                else if (firstArgument == "/p")      // Preview mode
+                {
+                    // TODO
+                }
+                else if (firstArgument == "/s")      // Full-screen mode
+                {
+                    List<MarcusImage> images = getImagesList();
+                    ShowScreenSaver(images);
+                    Application.Run();
+                }
+                else    // Undefined argument
+                {
+                    MessageBox.Show("Sorry, but the command line argument \"" + firstArgument +
+                        "\" is not valid.", "ScreenSaver",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else    // No arguments - treat like /c
+            {
+                // TODO
+            }
+
+        }
+
+        static void ShowScreenSaver(List<MarcusImage> images)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                ScreenSaverForm screensaver = new ScreenSaverForm(screen.Bounds);
+                screensaver.Show();
+
+                var mew = screensaver.CreateGraphics();
+
+                Bitmap do1 = new Bitmap(images[0].filepath);
+                mew.DrawImage(do1,1,1);
+                
+                
+            }
+        }
+
+        static List<MarcusImage> getImagesList()
+        {
+            List<MarcusImage> retimages = new List<MarcusImage>();
+
+            string mypath = @"D:\Users\Marcus\Pictures\Guys\mattrush";
+
+            foreach(var filename in Directory.EnumerateFiles(mypath))
+            {
+                if (Path.GetExtension(filename).ToLower() == ".jpg")
+                {
+                    Bitmap img = new Bitmap(filename);
+                    retimages.Add(new MarcusImage { filepath = filename, width = img.Width, height = img.Height });
+                    img.Dispose();
+                }
+            }
+
+            return retimages;
+        }
+
+    }
+
+    
+
+    
+}
---
+++ PhotoCollectionSaver/ScreenSaverForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoCollectionSaver
+{
+    public partial class ScreenSaverForm : Form
+    {
+        private Point mouseLocation;
+
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+
+        }
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+        }
+
+        private void ScreenSaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+
+        private void ScreenSaverForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
